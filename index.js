27c7984c6282882acd3210e08c0878937e7dfc03
nazar <nazar.karabinovych@gmail.com>
2017-05-01 20:42:57 -0500

    data base

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-var express = require ('express');
-var app = express ();
-var _ = require ('underscore');
-var fs = require ('fs');
+var MongoClient = require('mongodb').MongoClient;
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(express.static('public'));
-app.use(express.static('node_modules/jquery/dist'));
-app.use(express.static('node_modules/bootstrap'));
-app.set('view engine', 'ejs');
-
-
-
-
-app.listen(3000);
+MongoClient.connect('mongodb://localhost:27017', function(err, db) {
+	if (err) {
+		console.log('Error', err);
+	} else {
+  console.log("Connected correctly to server");
+   db.close();
+ }
+ 
+});
